5c40dbee3e0ae5005f8bc1b78334c2b196935c5e
Jonathan Crum <jcrum@theobogroup.com>
2023-12-06 11:38:19 -0500

    12/06/2023 11:38:19

--- dist/middleware/monday-webhook.middleware.js
+++ dist/middleware/monday-webhook.middleware.js
@@ -13,7 +13,7 @@ let MondayWebhookMiddleware = exports.MondayWebhookMiddleware = class MondayWebh
         res = res.status(200);
         if (Object.keys(req.body).includes("challenge")) {
             res.send(req.body);
-            return next("route");
+            // return next("route");
         }
         else {
             res.send();
